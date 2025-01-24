ea4127cbadde4649a245a5fe65d8ad2c88ee316d
Justin Sternberg <justin@dsgnwrks.pro>
2025-01-24 16:50:02 -0500

    Remove timestamp query var if it exists

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import './style.css'
 
+// Remove timestamp query parameter if it exists
+const url = new URL(window.location.href)
+if (url.searchParams.has('t')) {
+   url.searchParams.delete('t')
+   window.history.replaceState({}, '', url.toString())
+}
+
 const app = createApp(App)
 app.use(createPinia())
 app.mount('#app')
